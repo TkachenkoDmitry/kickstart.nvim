0bb0165b6da0bed6fafa5676f4cf5ca5d4e21741
Dmitrii Tkachenko <dmitrii.tkachenko@strv.com>
2023-10-09 11:18:20 +0200

    follow opened file in a file tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,7 +9,19 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+				},
+				-- in whatever position is specified in window.position
+				-- "open_current",  -- netrw disabled, opening a directory opens within the
+				-- window like netrw would, regardless of window.position
+				-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+				use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+				-- instead of relying on nvim autocmd events.
+			}
+		})
 	end,
 
 	vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', {
