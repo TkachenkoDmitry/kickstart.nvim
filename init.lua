7dfd77ec0eae2babeabe61a4195c5fab8ab22aab
Dmitrii Tkachenko <dmitrii.tkachenko@strv.com>
2023-10-10 11:52:15 +0200

    added alpha + persistance

--- init.lua
+++ init.lua
@@ -485,6 +485,7 @@ require('which-key').register {
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>b'] = { name = '[B]uffers', _ = 'which_key_ignore' },
+  ["<leader>q"] = { name = "+quit/session" },
 }
 
 -- mason-lspconfig requires that these setup functions are called in this order
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,2 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>bd', '<cmd>bd!<CR>', { desc = '[D]elete buffer' })
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
