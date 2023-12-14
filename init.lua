48d0ccab6e855d6fcbb2b592ce5f522d5736b48e
Dmitrii Tkachenko <dmitrii.tkachenko@strv.com>
2023-12-14 13:56:36 +0100

    feat: 80 chars column

--- init.lua
+++ init.lua
@@ -840,6 +840,7 @@ cmp.setup {
   config = function() require('config.snippets') end
 }
 require("core.keymaps")
+require("core.options")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
